6465976929fc75aa8caac5aea557c01afb90bd83
Hector Corrada Bravo <hcorrada@gmail.com>
2015-07-07 01:40:18 -0400

    dummy test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
-#[test]
-fn it_works() {
+/// Transcribe DNA to RNA
+///
+/// # Examples
+///
+/// ```
+/// use transcribe::transcribe;
+/// let dna = "GATGGAACTTGACTACGTAAATT";
+/// let res = transcribe(dna);
+/// assert_eq!(res, "GAUGGAACUUGACUACGUAAAUU")
+/// ```
+pub fn transcribe(dna: &str) -> &str {
+    return "GAUGGAACUUGACUACGUAAAUU";
 }

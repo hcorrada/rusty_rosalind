40eef1ece855d48e10c2b9a910ec79ab2f67387b
Hector Corrada Bravo <hcorrada@gmail.com>
2015-07-07 02:01:35 -0400

    write doc test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
-#[test]
-fn it_works() {
+/// Compute reverse complement
+///
+/// # Examples
+///
+/// ```
+/// use revcomp::revcomp;
+///
+/// let dna = "AAAACCCGGT";
+/// let res = revcomp(dna);
+/// assert_eq!(res, "ACCGGGTTTT");
+/// ```
+fn revcomp() {
+
 }

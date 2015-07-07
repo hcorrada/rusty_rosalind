e43fd5c1fb62cc021798ecf4eb4ac7b48003b897
Hector Corrada Bravo <hcorrada@gmail.com>
2015-07-07 13:41:09 -0400

    gc calculator done

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
-#[test]
-fn it_works() {
+/// compute gc content of a string
+///
+/// # Examples
+///
+/// ```
+/// use gc_content::gc_content;
+///
+/// let dna = "AGCTATAG";
+/// assert_eq!(gc_content(dna), 37.5);
+/// ```
+pub fn gc_content(dna: &str) -> f32 {
+    let mut count = 0.0;
+    for c in dna.chars() {
+        count += match c {
+            'C' | 'G' => 1.0,
+            'A' | 'T' => 0.0,
+            _ => unreachable!(),
+        }
+    }
+    return 100.0 * count / dna.len() as f32;
 }

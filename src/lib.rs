b9ba24eb3b4705fc07032712a9726b673b6c3d84
Hector Corrada Bravo <hcorrada@gmail.com>
2015-07-07 01:51:55 -0400

    input implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::Read;
+
 /// Transcribe DNA to RNA
 ///
 /// # Examples
@@ -11,4 +14,30 @@
 pub fn transcribe(dna: &str) -> String {
     let rna = dna.replace("T", "U");
     return rna;
+}
+
+/// Read problem input from file
+///
+/// # Examples
+///
+/// ```
+/// use transcribe::read_input;
+///
+/// let filename = "test.txt";
+/// let dna = "GATGGAACTTGACTACGTAAATT";
+/// let res = read_input(filename);
+/// assert_eq!(res, dna);
+/// ```
+pub fn read_input(filename: &str) -> String {
+    let mut fhandle = File::open(filename)
+        .ok()
+        .expect("Could not open file");
+
+    let mut dna = String::new();
+    fhandle.read_to_string(&mut dna)
+        .ok()
+        .expect("Unable to read");
+
+    let dna = dna.trim().to_string();
+    return dna;
 }

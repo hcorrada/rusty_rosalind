18e7ef389e598800da5b9fafe71e0c106c36b569
Hector Corrada Bravo <hcorrada@gmail.com>
2015-07-10 18:41:45 +0100

    stylistic changes and commenting

--- src/lib.rs
+++ src/lib.rs
@@ -17,8 +17,9 @@ use itertools::Itertools;
 /// assert_eq!(res, "20 12 17 21");
 /// ```
 pub fn make_output(counts: &Vec<i32>) -> String {
-    let res = counts.iter().join(" ");
-    return res;
+    // join the vector into a string separated
+    // by spaces
+    counts.iter().join(" ")
 }
 
 /// Read input file
@@ -35,16 +36,20 @@ pub fn make_output(counts: &Vec<i32>) -> String {
 /// assert_eq!(res, string);
 /// ```
 pub fn read_input(filename: &str) -> String {
+    // open file
     let mut fhandle = File::open(filename)
         .ok()
         .expect("Unable to open file");
 
+    // allocate string to read input to
     let mut string = String::new();
     fhandle.read_to_string(&mut string)
         .ok()
         .expect("Unable to read");
-    let string = string.trim().to_string();
-    return string;
+
+    // remove whitespace ('trim' returns a slice so need)
+    // convert back to a String
+    string.trim().to_string()
 }
 
 /// count nucleotides in a string
@@ -59,12 +64,16 @@ pub fn read_input(filename: &str) -> String {
 /// assert_eq!(res, vec![20, 12, 17, 21])
 /// ```
 pub fn count_nucleotides(string: &str) -> Vec<i32> {
+    // use a BTreeMap to order the keys of the hash so that
+    // keys are ordered (ACGT)
     let mut map = BTreeMap::new();
 
+    // add 1 to counter hash entry for each character
+    // in string
     for c in string.chars() {
         *map.entry(c).or_insert(0) += 1;
     }
 
-    let values: Vec<i32> = map.values().cloned().collect();
-    return values;
+    // return vector of nucleotide counts
+    map.values().cloned().collect()
 }

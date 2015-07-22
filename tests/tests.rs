9474d8fd0ff5f0cea59a390e9bdaa0002f4e63b1
Hector Corrada Bravo <hcorrada@gmail.com>
2015-07-22 08:38:56 -0400

    move test to doc

--- tests/tests.rs
+++
@@ -1,9 +0,0 @@
-extern crate approximate_matching;
-
-use approximate_matching::Combinator;
-
-#[test]
-fn combinations1() {
-    let combs: Vec<_> = (0..3).combinations(2).collect();
-    assert_eq!(combs, vec![(0,1), (0,2), (1,2)]);
-}

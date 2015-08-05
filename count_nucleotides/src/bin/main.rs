8fbaf70597d57edd7d9a4f05b95ebf0000161ab1
Hector Corrada Bravo <hcorrada@gmail.com>
2015-08-05 15:10:08 -0400

    move read input to library

--- count_nucleotides/src/bin/main.rs
+++ count_nucleotides/src/bin/main.rs
@@ -1,5 +1,41 @@
+extern crate rosalind_lib;
+
 use std::env;
-extern crate count_nucleotides;
+use std::fs::File;
+use std::io::Read;
+use itertools::Itertools;
+
+/// Read input file
+///
+/// # Examples
+///
+/// ```
+/// use count_nucleotides::read_input;
+///
+/// let filename = "test.txt";
+/// let string = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC";
+/// let res = read_input(filename);
+///
+/// assert_eq!(res, string);
+/// ```
+pub fn read_input(filename: &str) -> String {
+    // open file
+    let mut fhandle = File::open(filename)
+        .ok()
+        .expect("Unable to open file");
+
+    // allocate string to read input to
+    let mut string = String::new();
+    fhandle.read_to_string(&mut string)
+        .ok()
+        .expect("Unable to read");
+
+    // remove whitespace ('trim' returns a slice so need)
+    // convert back to a String
+    string.trim().to_string()
+}
+
+
 
 fn main() {
     let filename = env::args().nth(1)

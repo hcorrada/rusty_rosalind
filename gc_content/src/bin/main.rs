c222101c8d3b06899679bb458fd19701be2bae80
Hector Corrada Bravo <hcorrada@gmail.com>
2015-08-05 16:17:55 -0400

    start moving things around

--- gc_content/src/bin/main.rs
+++ gc_content/src/bin/main.rs
@@ -1,13 +1,24 @@
-extern crate gc_content;
+extern crate rosalind_lib;
 
 use std::env;
-use gc_content::runit;
+use rosalind_lib::io::parse_fast_file;
+use rosalind_lib::dna_utils::gc_content;
 
-fn main() {
+pub fn main() {
     let filename = env::args().nth(1)
         .expect("Need one argument");
 
-    let (id, gc) = runit(&filename);
-    println!("{}", id);
-    println!("{}", gc);
+    let records = parse_fasta_file(&filename);
+    let mut maxgc = -1.0 as f32;
+    let mut maxid = "";
+
+    for (id, dna) in &records {
+        let curgc = gc_content(&dna);
+        if curgc > maxgc {
+            maxgc = curgc;
+            maxid = id;
+        }
+    }
+    println!("{}", maxid);
+    println!("{}", maxgc);
 }
